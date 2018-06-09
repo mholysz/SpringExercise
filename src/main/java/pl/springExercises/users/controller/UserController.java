bbd1b8c255027da3352ba17a9c3a3358488c1861
Unknown <maciejholysz@gmail.com>
2018-06-09 12:20:11 +0200

    Master
    GitKraken

--- src/main/java/pl/springExercises/users/controller/UserController.java
+++ src/main/java/pl/springExercises/users/controller/UserController.java
@@ -1,10 +1,7 @@
 package pl.springExercises.users.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import pl.springExercises.users.dto.UserDto;
 import pl.springExercises.users.service.UserService;
 
@@ -30,4 +27,9 @@ public class UserController {
         return userService.getAllUsers();
     }
 
+
+    @PutMapping(value = "/{userId}")
+    public void updateUser(@RequestBody UserDto userDto, @PathVariable Long userId){
+        System.out.println(userDto + " " + userId);
+    }
 }
